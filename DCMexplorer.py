8b4532ac1593460d02e3ce7e50b06b2dcdb361e0
Javiera Jilberto Vallejos <javi.jv4@gmail.com>
2025-01-28 13:14:20 -0500

    initial files

---
+++ DCMexplorer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 18:08:25 2020
+
+@author: javijv4
+"""
+
+import pydicom as pdcm
+import os
+
+path = '/home/jilberto/University of Michigan Dropbox/Javiera Jilberto Vallejos/Projects/Desmoplakin/Models/Patients_v2/Unused Scans/AD-24/3131/'
+lista = os.listdir(path)
+
+new_list = []
+for l in lista:
+   try:
+       int(l)
+   except:
+       continue
+   new_list.append(l)
+lista = list(map(int, new_list))
+lista.sort()
+
+SA_scan = None
+for scan in lista:
+    folder = path + str(scan) + '/'
+    file = os.listdir(folder)[0]
+    try:
+        img = pdcm.dcmread(folder + file, force=True)
+        print(scan, img.SeriesDescription)
+    except:
+        continue
---
+++ process_dcm_folder.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+Created on 2024/08/06 09:23:42
+
+@author: Javiera Jilberto Vallejos
+'''
+
+import os
+import glob
+import numpy as np
+from subprocess import Popen
+import shutil
+import pydicom as pdcm
+
+main_fldr = 'test_data/'
+scan_fldr = f'{main_fldr}/4986/'
+nifti_name = 'SA'
+
+tmp_fldr = 'tmp/'
+if not os.path.exists(tmp_fldr):
+    os.makedirs(tmp_fldr)
+
+all_files = glob.glob(f'{scan_fldr}/*', recursive=True)
+
+# Read only dcm files
+dcm_files = []
+for file in all_files:
+    try:
+        img = pdcm.dcmread(file)
+        dcm_files.append(file)
+    except:
+        pass
+
+# Second check: Fixing study times
+study_times = []
+for scan in dcm_files:
+    img = pdcm.dcmread(scan)
+    study_times.append(int(img.StudyTime))
+
+times, counts = np.unique(study_times, return_counts=True)
+if len(times) > 1:
+    fix_time = times[np.argmax(counts)]
+    time = times[np.argmin(counts)]
+    ind_scans = np.where(study_times == time)[0]
+    print('Scan times of {} scans are different. Fixing it.'.format(len(ind_scans)))
+
+    for scan in dcm_files:
+        img = pdcm.dcmread(scan)
+        img.StudyTime = str(fix_time)
+        img.save_as(scan)
+
+# Third check: Make trigger time to be the same
+trigger_times = []
+for scan in dcm_files:
+    img = pdcm.dcmread(scan)
+    trigger_times.append(float(img.TriggerTime))
+
+times, counts = np.unique(trigger_times, return_counts=True)
+if len(times) > 1:
+    fix_time = times[np.argmax(counts)]
+    time = times[np.argmin(counts)]
+    ind_scans = np.where(trigger_times == time)[0]
+    print('Trigger times of {} scans are different. Fixing it.'.format(len(ind_scans)))
+
+    for scan in dcm_files:
+        name = os.path.basename(scan)
+        img = pdcm.dcmread(scan)
+        img.TriggerTime = str(fix_time)
+        img.save_as(f'{tmp_fldr}/{name}')
+
+# Convert to nii
+with open('dcm2niix.log', 'w') as f:
+    p = Popen(['dcm2niix', '-o', main_fldr, '-f', nifti_name, '-z', 'y', 'm', 'y', tmp_fldr])
+    p.wait()
+
+
+# Clean
+shutil.rmtree(tmp_fldr)
+
+# Find .json files and remove
+json_files = glob.glob(main_fldr + '/*.json', recursive=True)
+for json_file in json_files:
+    os.remove(json_file)
+
+print('Done!')
